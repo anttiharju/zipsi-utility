c39f3aaf379186d4d6f4d567d1d6b2dee0bc11da
Antti Harju <antti@harju.io>
2023-12-28 20:01:46 +0200

    Improve unzip

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 set -eu
 
+unzipped_tag="_unzipped_"
+
 # shellcheck disable=SC2207
-files=($(find . -type f -name "*.docx" -o -name "*.xlsx" -o -name "*.pptx"))
+files=($(find . -type f -not -path "*${unzipped_tag}*" -name "*.docx" -o -name "*.xlsx" -o -name "*.pptx"))
 
 calling_path="$(pwd)"
-for fullpath in "${files[@]}"											# ./path/to/a.file
+for fullpath in "${files[@]}"												# ./path/to/a.file
 do
-	dirname="$(dirname "$fullpath")"									# ./path/to
-	basename="$(basename -- "$fullpath")"								# a.file
-	filename="${basename%%.*}"											# a
-	extension="${basename#*.}"											# file
-	unzip_folder_path="${dirname}/${filename}_unzipped_${extension}"	# ./path/to/a_unzipped_file
-	file_to_unzip="${filename}.tmp"										# a.tmp
-	fullpath_to_unzip="${unzip_folder_path}/${file_to_unzip}"			# ./path/to/a_unzipped_file/a.tmp
+	dirname="$(dirname "$fullpath")"										# ./path/to
+	basename="$(basename -- "$fullpath")"									# a.file
+	filename="${basename%%.*}"												# a
+	extension="${basename#*.}"												# file
+	unzip_folder_path="${dirname}/${filename}${unzipped_tag}${extension}"	# ./path/to/a_unzipped_file
 
-	mkdir -p "$unzip_folder_path"
-	cp "$fullpath" "$fullpath_to_unzip"
+	mkdir -p "$unzip_folder_path" && cp "$fullpath" "$unzip_folder_path"
 	cd "$unzip_folder_path" || exit
-	unzip -qo "$file_to_unzip"
-	rm "$file_to_unzip"
+	unzip -qo "$basename" && rm "$basename"
 	cd "$calling_path" || exit
 done
 
-# Should increase diff readability
+
+# Increase diff readability by inserting newlines between <tags>
+# MS Office documents have a tendency to have all tags on the same line
+
 # shellcheck disable=SC2207
-files_to_add_extra_line_breaks_to=($(find . -type f -path "*_unzipped_*"))
-for file in "${files_to_add_extra_line_breaks_to[@]}"
+files_to_insert_newlines_to=($(find . -type f -path "*${unzipped_tag}*"))
+
+for file in "${files_to_insert_newlines_to[@]}"
 do
 	# sed doesn't play nice with some filenames such as "[Content_Types].xml"
 	perl -i -p -e 's/></>\n</g' "$file"
