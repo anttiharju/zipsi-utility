77a2fc3fd4ab74786581e350d755dffc4818c588
Antti Harju <antti@harju.io>
2023-12-28 19:26:34 +0200

    Port unzi.ps1 to bash
    The bash ports are developed to work on macOS, but will probably
    work just fine inside WSL.
    
    Tested that rezipping produces valid .docx files

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -eu
 
-# macOS bash is old (3.2) -> can't satisfy shellcheck.net
+# shellcheck disable=SC2207
 files=($(find . -type f -name "*.docx" -o -name "*.xlsx" -o -name "*.pptx"))
 
-for file in "${files[@]}"
+calling_path="$(pwd)"
+for fullpath in "${files[@]}"											# ./path/to/a.file
 do
-	echo "$file"
-	echo potato # to see we actually have distinct values in the array
+	dirname="$(dirname "$fullpath")"									# ./path/to
+	basename="$(basename -- "$fullpath")"								# a.file
+	filename="${basename%%.*}"											# a
+	extension="${basename#*.}"											# file
+	unzip_folder_path="${dirname}/${filename}_unzipped_${extension}"	# ./path/to/a_unzipped_file
+	file_to_unzip="${filename}.tmp"										# a.tmp
+	fullpath_to_unzip="${unzip_folder_path}/${file_to_unzip}"			# ./path/to/a_unzipped_file/a.tmp
+
+	mkdir -p "$unzip_folder_path"
+	cp "$fullpath" "$fullpath_to_unzip"
+	cd "$unzip_folder_path" || exit
+	unzip -qo "$file_to_unzip"
+	rm "$file_to_unzip"
+	cd "$calling_path" || exit
 done
