9cfc433879b6aea1146423e2930c168d35ece388
anttiharju <antti@harju.io>
2023-12-29 10:00:23 +0200

    Fix recursive (un)zipping
    Also introduce case insensitivity

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -eu
 
-unzipped_tag="_unzipped_"
-zipped_tag="_zipped"
+UNZIPPED="_unzipped_"
+ZIPPED="_zipped"
 
 # shellcheck disable=SC2207
 files=($(find . -type f \
-	-not -path "*${unzipped_tag}*" \
-	-not -path "*${zipped_tag}.*" \
-	-name "*.docx" -o \
-	-name "*.xlsx" -o \
-	-name "*.pptx" \
-	))
+	-not -ipath "*${ZIPPED}.*" \
+	-iname "*.docx" -or \
+	-not -ipath "*${ZIPPED}.*" \
+	-iname "*.xlsx" -or \
+	-not -ipath "*${ZIPPED}.*" \
+	-iname "*.pptx" ))
 
 calling_path="$(pwd)"
-for fullpath in "${files[@]}"												# ./path/to/file.extension
+for path in "${files[@]}"										# ./path/to/file.extension
 do
-	dirname="$(dirname "$fullpath")"										# ./path/to
-	basename="$(basename -- "$fullpath")"									# file.extension
-	filename="${basename%%.*}"												# file
-	extension="${basename#*.}"												# extension
-	unzip_folder_path="${dirname}/${filename}${unzipped_tag}${extension}"	# ./path/to/file_unzipped_extension (folder)
+	dirname="$(dirname "$path")"								# ./path/to
+	basename="$(basename -- "$path")"							# file.extension
+	filename="${basename%%.*}"									# file
+	extension="${basename#*.}"									# extension
+	unzip_path="${dirname}/${filename}${UNZIPPED}${extension}"	# ./path/to/file_unzipped_extension (folder)
 
-	mkdir -p "$unzip_folder_path" && cp "$fullpath" "$unzip_folder_path"
-	cd "$unzip_folder_path" || exit
+	mkdir -p "$unzip_path" && cp "$path" "$unzip_path"
+	cd "$unzip_path" || exit
 	unzip -qo "$basename" && rm "$basename"
 	cd "$calling_path" || exit
 done
@@ -32,7 +32,7 @@ done
 # MS Office documents have a tendency to have all tags on the same line
 
 # shellcheck disable=SC2207
-files_to_insert_newlines_to=($(find . -type f -path "*${unzipped_tag}*"))
+files_to_insert_newlines_to=($(find . -type f -ipath "*${UNZIPPED}*"))
 
 for file in "${files_to_insert_newlines_to[@]}"
 do
